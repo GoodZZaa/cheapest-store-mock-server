1eedef2823e474c4ba77a572e73bd81e2eb34f9b
Jang Donghyeok <ssjf409@naver.com>
2022-12-30 01:32:32 +0900

    first commmit

---
+++ src/main/java/com/goodzza/cheapeststore/CheapestStoreApplication.java
@@ -0,0 +1,13 @@
+package com.goodzza.cheapeststore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheapestStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheapestStoreApplication.class, args);
+	}
+
+}
