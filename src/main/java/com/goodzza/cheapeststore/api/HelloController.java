71484cb0fb5c7418aafa0dad83c75138fcd25565
Jang Donghyeok <ssjf409@naver.com>
2022-12-30 20:12:14 +0900

    add hello and swagger

--- src/main/java/com/goodzza/cheapeststore/api/HelloController.java
+++ src/main/java/com/goodzza/cheapeststore/api/HelloController.java
@@ -1,5 +1,9 @@
 package com.goodzza.cheapeststore.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Operation(summary = "test hello", description = "hello api example")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK !!"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    })
     @GetMapping
-    public String hello() {
-        return "hello";
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("hello");
     }
 }
