7ca76e53b46dff784b40ac01acd461fa510d1018
Jang Donghyeok <ssjf409@naver.com>
2023-01-26 21:42:23 +0900

    bug fix

--- src/main/java/com/goodzza/cheapeststore/api/controller/checkout/CheckoutController.java
+++ src/main/java/com/goodzza/cheapeststore/api/controller/checkout/CheckoutController.java
@@ -24,7 +24,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -69,7 +68,7 @@ public class CheckoutController {
             @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
     })
     @PostMapping
-    public ResponseEntity<Boolean> insertMartProduct(@Valid @RequestParam MartProductInsertRequest request) {
+    public ResponseEntity<Boolean> insertMartProduct(@Valid @RequestBody MartProductInsertRequest request) {
         return ResponseEntity.ok(Boolean.TRUE);
     }
 
